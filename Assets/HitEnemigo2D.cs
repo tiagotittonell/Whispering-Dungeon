6e753647f8f0b05d14628ff1016882bcb2224477
tiago <tiaguitobag@gmail.com>
2023-07-15 00:08:39 -0300

    Ataque Enemigo 2 / Inteligencia

---
+++ Assets/HitEnemigo2D.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitEnemigo2D : MonoBehaviour
+{
+
+    public int Daño;
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.CompareTag("Player"))
+        {
+
+            //if (coll.GetComponent<Megaman_X>().HP_Min > 0 && coll.GetComponent<Megaman_X>().damage_ == false)
+            //{
+            //    coll.GetComponent<Megaman_X>().ani.SetTrigger("damage");
+            //    coll.GetComponent<Megaman_X>().damage_ = true;
+
+            //    coll.GetComponent<Megaman_X>().audio_S.clip = coll.GetComponent<Megaman_X>().sonido[3];
+            //    coll.GetComponent<Megaman_X>().audio_S.Play();
+
+            //    if (transform.position.x > coll.transform.position.x)
+            //    {
+            //        coll.GetComponent<Megaman_X>().empuje = -3;
+            //        coll.transform.rotation = Quaternion.Euler(0, 0, 0);
+            //    }
+            //    else
+            //    {
+            //        coll.GetComponent<Megaman_X>().empuje = 3;
+            //        coll.transform.rotation = Quaternion.Euler(0, 180, 0);
+            //    }
+
+            //    coll.GetComponent<Megaman_X>().HP_Min -= Daño;
+            //}
+        }
+    }
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Brillo();
+        /*
+        coll.GetComponentInParent<Megaman_X>().audio_S.clip = coll.GetComponentInParent<Megaman_X>().sonido[3];
+        coll.GetComponentInParent<Megaman_X>().audio_S.Play();
+        */
+    }
+}
---
+++ Assets/RangoEnemigo2D.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangoEnemigo2D : MonoBehaviour
+{
+    public Animator ani;
+    public Enemigo3 enemigo;
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.CompareTag("Player"))
+        {
+           ani.SetBool("Caminar", false);
+           ani.SetBool("Correr", false);
+           ani.SetBool("Atacar", true);
+           enemigo.atacando = true;
+           GetComponent<BoxCollider2D>().enabled = false;            
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
