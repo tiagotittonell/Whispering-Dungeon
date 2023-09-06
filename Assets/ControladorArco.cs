00e2ea5f548f0f1ed5bf1110200f21f966ac5902
tiago <tiaguitobag@gmail.com>
2023-09-05 22:01:20 -0300

    Mejora de ataque, Enemigo 2// Inteligencia

---
+++ Assets/ControladorArco.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorArco : MonoBehaviour
+{
+    //private SpriteRenderer spriteRenderer;
+
+    //public new Camera camera;
+    //public Transform spawner;
+    //public GameObject flechaPrefab;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    spriteRenderer= GetComponent<SpriteRenderer>(); 
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    //private void RotateTowardMouse()
+    //{
+    //    float angle;
+    //    transform.rotation= Quaternion.Euler(0,0,angle);
+    //    spriteRenderer.flipY = angle >= 90 && angle <= 270;
+    //}
+
+    //private float GetAngleTowardsMouse()
+    //{
+    //    Vector3 mouseWorldPosition = camera.ScreenToViewportPoint(Input.mousePosition);
+    //    Vector3 mouseDirection = mouseWorldPosition - transform.position;
+    //    float angle = (Vector3.SignedAngle(Vector3.right, mouseDirection));
+    //}
+}
---
+++ Assets/Scripts/RangoEnemigo2D.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangoEnemigo2D : MonoBehaviour
+{
+    public Animator ani;
+    public COPIAENEMIGOSCRIPT enemigo;
+    public float rnd = 0;
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+
+        if (coll.CompareTag("PJ"))
+        {
+
+            ani.SetBool("Caminar", false);
+            ani.SetBool("Correr", false);
+            rnd = Random.Range(0.5f, 0.75f);
+            StartCoroutine("Cooldown");
+
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(rnd);
+        ani.SetBool("Ataque", true);
+        enemigo.atacando = true;
+        GetComponent<BoxCollider2D>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //public Animator ani;
+    //public Enemigo3 enemigo; // Asigna el componente del script del enemigo aquí
+
+    //void Start()
+    //{
+    //    ani.SetBool("Ataque", true); // Activar la animación de ataque al inicio
+    //}
+
+    //void OnTriggerEnter2D(Collider2D coll)
+    //{
+    //    if (coll.CompareTag("PJ"))
+    //    {
+    //        ani.SetBool("Caminar", false);
+    //        ani.SetBool("Correr", false);
+    //        enemigo.atacando = true;
+    //        enemigo.RealizarAtaque(); // Llamamos al método de RealizarAtaque() en el enemigo
+    //        GetComponent<BoxCollider2D>().enabled = false;
+    //    }
+    //}
+
+    //void OnTriggerExit2D(Collider2D coll)
+    //{
+    //    if (coll.CompareTag("PJ"))
+    //    {
+    //        enemigo.DetenerAtaque(); // Llamamos al método para detener el ataque del enemigo
+    //        GetComponent<BoxCollider2D>().enabled = true;
+    //    }
+    //}
+    //public Animator ani;
+    //public Enemigo3 enemigo;
+
+    //void OnTriggerEnter2D(Collider2D coll)
+    //{
+    //    if (coll.CompareTag("PJ"))
+    //    {
+    //        ani.SetBool("Caminar", false);
+    //        ani.SetBool("Correr", false);
+    //        ani.SetBool("Ataque", true);
+    //        enemigo.atacando = true;
+    //        GetComponent<BoxCollider2D>().enabled = false;
+    //    }
+    //}
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+}
